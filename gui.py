287f14239e0fb5ff495b9785c6fd0a3ed5668ced
Naturarerum <naturarerum@yahoo.fr>
2025-12-12 23:10:02 -0500

    interface avec les widgets

--- gui.py
+++ gui.py
@@ -1,12 +1,12 @@
 from PySide6 import QtWidgets
 from PySide6.QtCore import QSize
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QListWidget, QLineEdit, QWidget
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("")
+        self.setWindowTitle("PyTasks")
         self.setMinimumSize(QSize(400, 300))
         # Initialisation de l'UI
         self.setup_ui()
@@ -15,20 +15,27 @@ class MainWindow(QtWidgets.QMainWindow):
         self.create_widgets()
         self.modify_widgets()
         self.create_layouts()
+        self.central_widget = QWidget()
+        self.central_widget.setLayout(self.main_Layout)
+        self.setCentralWidget(self.central_widget)
         self.add_widgets_to_layouts()
         self.setup_connections()
 
     def create_widgets(self):
-        pass
+        self.lst_tasks = QListWidget()
+        self.lne_task_name = QLineEdit()
+        self.btn_add_task = QPushButton("tout supprimer")
 
     def modify_widgets(self):
         pass
 
     def create_layouts(self):
-        pass
+        self.main_Layout = QVBoxLayout()
 
     def add_widgets_to_layouts(self):
-        pass
+        self.main_Layout.addWidget(self.lst_tasks)
+        self.main_Layout.addWidget(self.lne_task_name)
+        self.main_Layout.addWidget(self.btn_add_task)
 
     def setup_connections(self):
         pass
--- tasks.py
+++ tasks.py
@@ -1,14 +1,26 @@
-
-
 class Tasks:
 
-    def __init__(self, task_name):
-        self.task_name = task_name
+    def __init__(self):
         self.tasks_list = []
 
-    def create_task(self):
-        self.tasks_list.append(self.task_name)
+    def create_task(self, task_name):
+        self.tasks_list.append(task_name)
 
-    def delete_task(self):
-        pass
-    
+    def delete_task(self, task_name):
+        for task in self.tasks_list:
+            if task_name == task:
+                self.tasks_list.remove(task)
+
+    def delete_all_task(self):
+        self.tasks_list.clear()
+
+
+if __name__ == '__main__':
+    t1 = Tasks()
+    t1.create_task("apprendre le latin")
+    t1.create_task("sport")
+    t1.create_task("faire le menage")
+    print(t1.tasks_list)
+    t1.delete_task("sport")
+    t1.delete_all_task()
+    print(t1.tasks_list)
